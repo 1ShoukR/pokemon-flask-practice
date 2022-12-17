78398e368cc0d0d88f709383d8150eacaca4a740
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-17 17:51:38 -0500

    created frontend for pomemon app

--- models.py
+++ models.py
@@ -8,12 +8,11 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(80), unique = True, nullable = True)
-    password = db.Column(db.String(120), nullable = True)
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=True)
+    password = db.Column(db.String(120), nullable=True)
 class Pokemon(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, nullable = False)
-    pokemon_name = db.Column(db.string(120), nullable = False)
-    
+    id = db.Column(db.Integer, primary_key=True) # can make foreign key
+    user_id = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text(), nullable=True)
