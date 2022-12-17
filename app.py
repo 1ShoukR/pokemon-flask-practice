b90da409395f56aef937b3b679d2b495ad309639
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-17 00:40:42 -0500

    created classes for DB objects. also started creating routes to add pokemon to

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, session, g
-# from models import db, User, ToDO
+from models import db, User, Pokemon
 from flask_cors import CORS, cross_origin
 import json
 import os
@@ -11,9 +11,11 @@ app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = '\xbb\xe7A\xe3\x9d=tg\xf6\x86).\xf8\xb1\x82L_99\x00\xc1\xc1\xf3\x95'
 app.config['DEBUG'] = True
-
 # Database
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Rahmin12@localhost:3306/pokemon_api'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
+db.init_app(app)
 
 
 
@@ -27,7 +29,9 @@ def index():
 
 
 
-
+@app.route("/add-pokemon")
+def add_pokemon():
+    pass
 
 
 
--- models.py
+++ models.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique = True, nullable = True)
+    password = db.Column(db.String(120), nullable = True)
+
+class Pokemon(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, nullable = False)
+    pokemon_name = db.Column(db.string(120), nullable = False)
+    
