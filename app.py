db26fd8e868b4b7db5e65c2f956eab3c8fea7494
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-16 23:09:20 -0500

    created initial flask backend for pokemon app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, render_template, request, session, g
+from models import db, User, ToDO
+from flask_cors import CORS, cross_origin
+import json
+import os
+from datetime import datetime
+
+
+# Flask
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = '\xbb\xe7A\xe3\x9d=tg\xf6\x86).\xf8\xb1\x82L_99\x00\xc1\xc1\xf3\x95'
+app.config['DEBUG'] = True
+
+# Database
