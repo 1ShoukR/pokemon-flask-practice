506066e957e182d2ab90c16e9b47cc2b09dd54e8
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-16 23:22:23 -0500

    got the backend up and running

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, session, g
-from models import db, User, ToDO
+# from models import db, User, ToDO
 from flask_cors import CORS, cross_origin
 import json
 import os
@@ -13,3 +13,30 @@ app.config['SECRET_KEY'] = '\xbb\xe7A\xe3\x9d=tg\xf6\x86).\xf8\xb1\x82L_99\x00\x
 app.config['DEBUG'] = True
 
 # Database
+
+
+
+
+
+# Routes
+
+@app.route("/")
+def index():
+    return "I am working"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
