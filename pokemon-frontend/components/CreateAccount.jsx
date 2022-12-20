51d35349ebc299a5d28a7f571ad19d607058153d
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-19 20:08:11 -0500

    created create account function to communicate with backend

--- pokemon-frontend/components/CreateAccount.jsx
+++ pokemon-frontend/components/CreateAccount.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
 const CreateAccount = () => {
-  return <div>CreateAccount</div>;
+  const handleCreateAccountClick = (e) => {
+    e.preventDefault()
+    let input = {
+      username: e.target.form[0].value,
+      password: e.target.form[1].value
+    }
+    console.log(input)
+    // function is working. now just need to post to backend and database.
+  }
+  return (
+		<div>
+			<div>
+				<h1>Create an Account</h1>
+			</div>
+			<div>
+				<form >
+					<div>
+						<input type="username" placeholder="username" />
+						<input type="password" placeholder="password" />
+					</div>
+					<div>
+						<button onClick={handleCreateAccountClick}>create an account</button>
+					</div>
+				</form>
+			</div>
+			<div>
+				<h1>Already have an account? Login here.</h1>
+			</div>
+			<div>
+				<form>
+					<div>
+						<input type="username" placeholder="username" />
+						<input type="password" placeholder="password" />
+					</div>
+          <div>
+            <button>Login</button>
+          </div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default CreateAccount;
