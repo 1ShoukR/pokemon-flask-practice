955a73167c90aad6ded4922d7cd4f75ee081e7dc
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-19 20:10:25 -0500

    Created login function to communicate to the backend

--- pokemon-frontend/components/CreateAccount.jsx
+++ pokemon-frontend/components/CreateAccount.jsx
@@ -10,6 +10,15 @@ const CreateAccount = () => {
     console.log(input)
     // function is working. now just need to post to backend and database.
   }
+  const handleLoginClick = (e) => {
+		e.preventDefault();
+		let input = {
+			username: e.target.form[0].value,
+			password: e.target.form[1].value,
+		};
+		console.log(input);
+		// function is working. now just need to post to backend and database.
+	}
   return (
 		<div>
 			<div>
@@ -36,7 +45,7 @@ const CreateAccount = () => {
 						<input type="password" placeholder="password" />
 					</div>
           <div>
-            <button>Login</button>
+            <button onClick={handleLoginClick}>Login</button>
           </div>
 				</form>
 			</div>
